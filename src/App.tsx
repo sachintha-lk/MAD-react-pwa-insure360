c3285fb0198b6536a68131c98960dd86fca11f76
Sachintha Lakmin <68807141+sachintha-lk@users.noreply.github.com>
2023-08-22 14:52:53 +0530

    vite-pwa-plugin, chakra, react setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Text } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <>
+      <Text></Text>
+    </>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from "vite-plugin-pwa";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(),  VitePWA({
+    registerType: 'autoUpdate',
+    devOptions: {
+      enabled: true
+    },
+    manifest: {
+      name: 'My Awesome App',
+      short_name: 'MyApp',
+      description: 'My Awesome App description',
+      theme_color: '# ',
+      icons: [
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ],
+      start_url: "/",
+      display: "fullscreen",
+      background_color: "#B12A34"
+    }
+  })],
+})
